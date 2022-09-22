5453c9546d0c37d176b74ff93617da8b9d770498
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-09-22 11:54:23 -0300

    subindo bd

---
+++ bd/ddl.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE shintechDB;
+USE shintechDB;
+
+CREATE TABLE TB_USUARIO (
+id_usuario      int primary key auto_increment,
+nm_usuario      varchar(50),
+dt_nascimento   datetime,
+ds_email        varchar(99),
+ds_senha        varchar(20),
+nr_cpf          int(11)
+);
+
+CREATE TABLE TB_LOGIN (
+id_login        int primary key auto_increment,
+id_usuario      int,
+FOREING KEY (id_usuario) references TB_USUARIO(id_usuario)
+);
+
+// 113.55
