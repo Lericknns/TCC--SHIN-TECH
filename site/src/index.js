06def1162632595a05b2eaca57f897fa8f4f3a63
Leandro Geraldo Nunes dos Santos <gerakleandro@gmail.com>
2022-09-27 17:31:29 -0300

    fail

--- site/src/index.js
+++ site/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Routes from './src/routes.js';
+import Routes from './routes';
 
-import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
