883654d3531f7fcd6bb0d8833f1fecba4480bf90
Leandro Nunes <gerakleandro@email.com>
2022-10-04 08:46:00 -0300

    new

--- site/src/pages/User/telaUser/index.jsx
+++ site/src/pages/User/telaUser/index.jsx
@@ -3,6 +3,7 @@ import './index.scss';
 export default function telaUser(){
     <main>
         <section>
+            <h1>teste</h1>
             <img src='./images/logo.png' alt='' />
         </section>
     </main>
