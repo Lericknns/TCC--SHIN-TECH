6141446a3e263d67fd253a45c6d5c1dc46c61a85
Leandro Nunes <gerakleandro@email.com>
2022-10-05 09:44:30 -0300

    consertei essa merda

--- site/src/pages/User/usuariohome/index.jsx
+++ site/src/pages/User/usuariohome/index.jsx
@@ -2,9 +2,9 @@ import './index.scss'
 export default function Index() {
 
     return(
-       <main>
-        <section>
-        <img src='./images/logo.png' alt='' />
+       <main className='Home'>
+        <section className='sec'>
+        <img className='img' src='./images/logo.png' alt='' />
         <h1>aaa</h1>  
         </section>
        </main>
