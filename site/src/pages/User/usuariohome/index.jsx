1f5809f5787b4f7b41d3f14afb23c2dfe0c00e23
Leandro Nunes <gerakleandro@email.com>
2022-10-06 11:46:16 -0300

    avanços

--- site/src/pages/User/usuariohome/index.jsx
+++ site/src/pages/User/usuariohome/index.jsx
@@ -3,12 +3,15 @@ export default function Index() {
 
     return(
        <main className='Home'>
+
         <section className='sec'>
-        <img className='img' src='./images/logo.png' alt='' />
+        <img className='img' src='./assets/images/logo.png' alt='' />
         <h1 className='text'>loja</h1>
-        <h1>sobre</h1>  
-        <h1>suporte</h1>
+        <h1 className='text'>sobre</h1>  
+        <h1 className='text'>suporte</h1>
         </section>
+        <hr/>
+        <section></section>
        </main>
     )
 }
