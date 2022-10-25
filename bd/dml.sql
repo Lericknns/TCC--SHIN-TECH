df3a7b03f12c941973477a408f1dcdd56f6c7ea8
denner trindade de souza <trindadesouzadenner@gmail.com>
2022-10-25 09:47:14 -0300

    a

--- bd/dml.sql
+++ bd/dml.sql
@@ -1,10 +1,21 @@
---- Login do Usuário
+use shintech;
 
-SELECT id_usuario       id,
-       ds_email         email
-FROM TB_USUARIO
-WHERE ds_email          ='admin@admin.com.br'
-AND   ds_senha          ='admin1234'
-
-INSERT INTO (nm_usuario, ds_email, ds_senha)
-     VALUES ('Arthur', 'admin@admin.com.br', 'batatatinhamane');
+insert into tb_usuario ( 
+	ds_email,
+    ds_senha,
+    nm_usuario,
+    dt_nascimento)
+    value ("NICHOLAS@gmail.com", 'dener', 'leandro', "2005-11-11");
+    
+    insert into tb_login (	
+    id_usuario ,
+    ds_email ,
+    ds_senha)
+    value ('1', 'trindadesouzadenner@gmail.com' , "14233221" );
+    
+    
+    insert into tb_favorito (
+    id_usuario_favorito,
+    id_produto,
+    
+    value ('');
